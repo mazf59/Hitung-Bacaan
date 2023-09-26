fa9965a9f55935ed7727ec89cdc53d93d7082dde
mazf59 <squaredzam@gmail.com>
2023-09-26 22:54:34 +0700

    update yu ai

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-function hitungRencanaBaca() {
-    const sisaHalaman = parseInt(document.getElementById("halaman").value);
-    const tanggalMulai = new Date(document.getElementById("mulai").value);
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("readingCalculatorForm").addEventListener("submit", hitungRencanaBaca);
+    document.querySelectorAll("[name='jenis-tanggal-akhir']").forEach(elem => {
+        elem.addEventListener("change", gantiTanggalAkhir);
+    });
+});
+
+function hitungRencanaBaca(event) {
+    event.preventDefault();
+
+    const halamanElem = document.getElementById("halaman");
+    const mulaiElem = document.getElementById("mulai");
+    const akhirElem = document.getElementById("akhir");
+    const hariElem = document.getElementById("hari");
+    const hasilElem = document.getElementById("hasil");
+
+    const sisaHalaman = parseInt(halamanElem.value);
+    const tanggalMulai = new Date(mulaiElem.value);
     let tanggalAkhir;
+
     if (document.getElementById("tanggal-akhir").checked) {
-        tanggalAkhir = new Date(document.getElementById("akhir").value);
+        tanggalAkhir = new Date(akhirElem.value);
     } else {
         tanggalAkhir = new Date(tanggalMulai.getTime());
-        tanggalAkhir.setDate(tanggalAkhir.getDate() + parseInt(document.getElementById("hari").value));
+        tanggalAkhir.setDate(tanggalMulai.getDate() + parseInt(hariElem.value));
     }
-    const hasil = document.getElementById("hasil");
 
     if (isNaN(sisaHalaman) || isNaN(tanggalMulai.getTime()) || isNaN(tanggalAkhir.getTime()) || sisaHalaman <= 0 || tanggalAkhir <= tanggalMulai) {
-        hasil.innerHTML = "Input tidak valid.";
-        hasil.style.color = "red";
+        hasilElem.innerText = "Input tidak valid.";
+        hasilElem.style.color = "red";
         return;
     }
 
     const hariAntara = (tanggalAkhir - tanggalMulai) / (1000 * 60 * 60 * 24) + 1;
     const halamanPerHari = Math.ceil(sisaHalaman / hariAntara);
-    hasil.innerHTML = `Baca ~${halamanPerHari} halaman/hari.`;
-    hasil.style.color = "green";
+    hasilElem.innerText = `Baca ~${halamanPerHari} halaman/hari.`;
+    hasilElem.style.color = "green";
 }
 
 function gantiTanggalAkhir() {
-    document.getElementById("group-tanggal-akhir").classList.toggle('hidden');
-    document.getElementById("group-hari").classList.toggle('hidden');
+    const groupTanggalAkhir = document.getElementById("group-tanggal-akhir");
+    const groupHari = document.getElementById("group-hari");
+    groupTanggalAkhir.classList.toggle('hidden');
+    groupHari.classList.toggle('hidden');
+    
+    const akhirElem = document.getElementById("akhir");
+    const hariElem = document.getElementById("hari");
+    
+    akhirElem.required = !akhirElem.required;
+    hariElem.required = !hariElem.required;
 }
